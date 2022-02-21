6f0beedcf0cf64ad89c223272b9abfdfc5dd43cc
Adanato <66979547+Adanato@users.noreply.github.com>
2022-02-21 11:49:58 -0500

    Cleaned up functions. added player stats
    Multiple functions were affecting the global fail variable. FailCheck() and PlayAgain are the only functions able to affect it now.
    
    Moved Rounds input() to its own function(RoundsInput) to run a try and except handler to prevent users from crashing the program.
    
    Added new variables in the RockPaperScissors function to track stats.
    
    Added modded Winner() and Loser() and added Tied() to track stats in RockPaperScissors Function.

--- RabboRockPaperScissors.py
+++ RabboRockPaperScissors.py
@@ -2,7 +2,6 @@
 import random
 #User only has 5 chances before Rabbo the Rat terminates their lifespan.
 Fail = 0
-Games = 0
 
 #Offers the player the choice to keep playing or quit the game.
 def PlayAgain():
@@ -20,6 +19,8 @@ def PlayAgain():
             
 #Checks the users fail count before having them restart or quit the game.            
 def FailCheck():
+    global Fail
+    Fail += 1
     if Fail > 5:
         print("Nvm You suck at rock paper scissors. Hmph!. *Rabbo pulls out his glock and kills you.")
         print("You Died")
@@ -35,7 +36,6 @@ def RabboChoice():
 #If the user is wrong the function will keep calling itself.
 
 def UserChoice():
-    global Fail
     
     Action = str(input("Rock, Paper, or Scissors?\n"))
     match Action:
@@ -46,27 +46,55 @@ def UserChoice():
         case "Scissors":
             return "Scissors"
         case _:
-            Fail += 1
             FailCheck()
             print("Type it properly NOW")
             UserChoice()
             
 #To be inserted inside the RockPaperScissors Function.
-#Tells you if you won using the variables from the RPS Function.           
+#Tells you if you won using the variables from the RPS Function and returns a 1 for the wins stat.           
 def Winner(P,R):
     print("You picked " + P + " and Rabbo picked " + R)
-    print("You won!")
+    print("You Won!")
+    return 1
     
 #To be inserted inside the RockPaperScissors Function.
-#Tells you if you lost using the variables from the RPS Function.
+#Tells you if you lost using the variables from the RPS Function and returns a 1 for the losses stat.
 def Loser(P,R):
     print("You picked " + P + " and Rabbo picked " + R)
     print("You Lost!")
+    return 1
 
+##To be inserted inside the RockPaperScissors Function.
+#Tied function tells the user if they tied and returns a 1 for the ties stat.
+def Tied():
+    print("We tied, darn it!")
+    return 1
 #To be inserted inside the RockPaperScissors Function.
 #This function checks if the amount of rounds the user requested has been met or not.
 def RoundsChecker(Rounds):
-    print("We have " + str(Rounds) + " Rounds left") 
+    print("We have " + str(Rounds) + " Rounds left")
+    return 1 
+
+#Checks the user's input to make sure it is a valid integer. 
+#If not it forces the user to input a valid integer.
+#Prevents the user from playing too long.
+def RoundsInput():
+    Rounds = input("How many times do you want to play?\n")
+    try:
+        Rounds = int(Rounds)
+
+        if Rounds > 10:
+            print("I'm not playing that long hmph")
+            RoundsInput()
+        elif Rounds == 0:
+            print("What do you mean 0! Pick a proper amount")
+            RoundsInput()
+        else:
+            print("Cool lets get started.")
+        return Rounds
+    except ValueError:
+        print("Type a proper number you kindergartener")
+        RoundsInput()
 
 #Compares the stat of the User and the Cpu.
 #Gives the final results at the end.
@@ -74,40 +102,45 @@ def RoundsChecker(Rounds):
     
 #Main Function that operates the game by comparing the player and Rabbo's actions.    
 def RockPaperScissors():
-    Rounds = int(input("How many times do you want to play?\n"))
-    if Rounds > 10:
-        print("I'm not playing that long idiot")
+    PlayerL= 0
+    PlayerW = 0
+    PlayerT= 0
+
+    Rounds = RoundsInput()
+
     while Rounds > 0:
         Player = UserChoice()
         Rabbo = RabboChoice()
     
         if Player == Rabbo:
-            print("Dang it we tied")
-            Rounds -= 1
-            RoundsChecker(Rounds)
+            PlayerT += Tied()
+            Rounds -= RoundsChecker(Rounds)
+            
         elif Player == "Rock" and Rabbo == "Scissors":
-            Winner(Player,Rabbo)
-            Rounds -= 1
-            RoundsChecker(Rounds)
+            PlayerW += Winner(Player,Rabbo)
+            Rounds -= RoundsChecker(Rounds)
+            
         elif Player == "Scissor" and Rabbo == "Paper":
-            Winner(Player,Rabbo)
-            Rounds -= 1
-            RoundsChecker(Rounds)
+            PlayerW += Winner(Player,Rabbo)
+            Rounds -= RoundsChecker(Rounds)
+            
         elif Player == "Paper" and Rabbo == "Rock":
-            Winner(Player,Rabbo)
-            Rounds -= 1
-            RoundsChecker(Rounds)
+            PlayerW += Winner(Player,Rabbo)
+            Rounds -= RoundsChecker(Rounds)
+            
         else:
-            Loser(Player,Rabbo)
-            Rounds -= 1
-            RoundsChecker(Rounds)
-    
+            PlayerL += Loser(Player,Rabbo)
+            Rounds -= RoundsChecker(Rounds)
+
+    print("You tied " + str(PlayerT) + " times")        
+    print("You won " + str(PlayerW) + " times")        
+    print("You lost " + str(PlayerL) + " times")
     print("Huhu thanks for playing that was fun")
     PlayAgain()
 #Long list of Functions end here.
 
         
-#Start of the game    
+#Start Of The Game# 
 print("Hi ugly rat what is your name?")
 UserName = input()
 
